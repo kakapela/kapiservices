a62e510b7b0f0395844aa637c6980199c652ff18
kakapela <kacperkapela1@gmail.com>
2023-07-20 18:34:53 +0200

    package all microservices to docker containers. Setting up connection between containers

--- amqp/src/main/java/com/amqp/config/RabbitMQMessageProducer.java
+++ amqp/src/main/java/com/amqp/config/RabbitMQMessageProducer.java
@@ -2,7 +2,7 @@ package com.amqp.config;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,12 +10,12 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class RabbitMQMessageProducer {
 
-    private final AmqpTemplate amqpTemplate;
+    private final RabbitTemplate rabbitTemplate;
 
     public void publish(Object payload, String exchange, String routingKey)
     {
         log.info("Publishing to {} using routingKey {}. Payload: {}", exchange, routingKey, payload);
-        amqpTemplate.convertAndSend(exchange, routingKey, payload);
+        rabbitTemplate.convertAndSend(exchange, routingKey, payload);
         log.info("Published to {} using routingKey {}. Payload: {}", exchange, routingKey, payload);
     }
 }
