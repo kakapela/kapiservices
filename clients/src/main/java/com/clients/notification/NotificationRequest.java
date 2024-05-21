af3c05f8d6b2cec201596bc232063cd087235398
kakapela <kacperkapela1@gmail.com>
2024-05-21 22:22:11 +0200

    Refactoring microservices for k8s

--- clients/src/main/java/com/clients/notification/NotificationRequest.java
+++ clients/src/main/java/com/clients/notification/NotificationRequest.java
@@ -1,8 +1,10 @@
 package com.clients.notification;
 
+import java.io.Serializable;
+
 public record NotificationRequest(
         Integer toCustomerId,
         String toCustomerName,
         String message
-) {
+) implements Serializable {
 }
